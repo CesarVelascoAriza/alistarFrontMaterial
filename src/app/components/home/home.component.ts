b829de570673bbc897b2ff1cff06fe58f1c05957
cesar <caacesar.ariza@gmail.com>
2021-06-18 20:25:48 -0500

    cambios
    cambios que no me acuerdo de que son

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -7,9 +7,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  slideIndex: number = 1
+  constructor() {
 
-  ngOnInit(): void {
   }
 
+  ngOnInit(): void {
+    console.log(this.slideIndex)
+    this.showSlides(this.slideIndex);
+  }
+
+
+ plusSlides(n:any) {
+  this.showSlides(this.slideIndex += n);
+}
+
+ currentSlide(n:any) {
+  this.showSlides(this.slideIndex = n);
+}
+
+ showSlides(n:any) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) { this.slideIndex = 1 }
+  if (n < 1) { this.slideIndex = slides.length }
+  for (i = 0; i < slides.length; i++) {
+
+    slides[i].className += ' display'
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[this.slideIndex - 1].className.replace('display','pruba');
+  dots[this.slideIndex - 1].className += " active";
+}
 }
