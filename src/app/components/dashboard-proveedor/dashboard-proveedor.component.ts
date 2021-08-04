b6096da772388a11697578d67847b9d578700c97
JohanaLl <johisllanos@gmail.com>
2021-08-03 19:31:40 -0500

    tarjetas

--- src/app/components/dashboard-proveedor/dashboard-proveedor.component.ts
+++ src/app/components/dashboard-proveedor/dashboard-proveedor.component.ts
@@ -9,10 +9,14 @@ import { DashboardService } from 'src/app/services/dashboard.service';
 export class DashboardProveedorComponent implements OnInit {
   
   pieChart: [] | any;
+  serviciosSolicitados: [] | any;
+  proximosEventos: [] | any;
 
   constructor(private dashboardService: DashboardService) {}
 
   ngOnInit(): void {
     this.pieChart = this.dashboardService.pieChart();
+    this.serviciosSolicitados = this.dashboardService.serviciosSolicitados();
+    this.proximosEventos = this.dashboardService.proximosEventos();
   }
 }
--- src/app/layouts/widgets/card/card.component.ts
+++ src/app/layouts/widgets/card/card.component.ts
@@ -1,6 +1,4 @@
 import { Component, Input, OnInit } from '@angular/core';
-import * as Highcharts from 'highcharts';
-import HC_exporting from 'highcharts/modules/exporting';
 
 @Component({
   selector: 'app-widget-card',
@@ -10,60 +8,11 @@ import HC_exporting from 'highcharts/modules/exporting';
 export class CardComponent implements OnInit {
 
   @Input() label: string = "";
-  @Input() listaServicios: [] = [];
-  
-  Highcharts = Highcharts;
-  chartOptions = {}
-
-  @Input() data = [];
+  @Input() data: [] = [];
   
   constructor() { }
 
   ngOnInit(): void {
-    this.chartOptions = {
-      chart: {
-          type: 'area',
-          backgroundColor: null,
-          borderWidth: 0,
-          margin: [2, 2, 2, 2],
-          height: 60
-      },
-      title: {
-          text: null
-      },
-      subtitle: {
-          text: null
-      },
-      tooltip: {
-          split: true,
-          outside: true
-      },
-      legend: {
-        enabled: false,
-      },
-      credits: {
-        enabled: false,
-      },
-      exporting: {
-        enabled: false,
-      },
-      xAxis : {
-        labels: {
-           enabled: false
-        },
-        title: {
-          text: null
-        },
-        startOnTick: false,
-        endOnTick: false,
-        tickOptions: []
-      },
-      series: [{
-        data: this.data
-      }]
-    };
-
-    HC_exporting(Highcharts);
     
     setTimeout(() => {
       window.dispatchEvent(
