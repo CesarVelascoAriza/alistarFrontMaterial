fef90f77dc9e2a774925f7bb1dfdfbabc8a5588a
cesar <caacesar.ariza@gmail.com>
2021-10-12 23:40:37 -0500

    Auto stash before merge of "master" and "origin/master"

--- src/app/components/dashboard-user/dashboard-user.component.ts
+++ src/app/components/dashboard-user/dashboard-user.component.ts
@@ -2,36 +2,74 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { map } from 'rxjs/operators';
 import { Breakpoints, BreakpointObserver } from '@angular/cdk/layout';
 import { MediaObserver, MediaChange } from '@angular/flex-layout'
-import { Subscription } from  'rxjs'
+import { Subscription } from 'rxjs'
+import { UsuarioService } from 'src/app/services/usuario.service';
+import { Usuario } from 'src/app/models/usuario';
+import { TipoDocumento } from 'src/app/models/tipoDocumento';
+import { DropDownService } from 'src/app/services/drop-down.service';
+import { FormBuilder, Validators } from '@angular/forms';
+
+
 
 @Component({
   selector: 'app-dashboard-user',
   templateUrl: './dashboard-user.component.html',
   styleUrls: ['./dashboard-user.component.css']
 })
-export class DashboardUserComponent implements OnInit, OnDestroy{
+export class DashboardUserComponent implements OnInit, OnDestroy {
 
-  showFiller = false;
-  mediasub: Subscription | undefined;
-  devices: boolean | undefined;
-  pieChart: [] | any;
-  serviciosSolicitados: [] | any;
-  proximosEventos: [] | any;
 
-  constructor(
-    private breakpointObserver: BreakpointObserver,
-    public mediaObserver: MediaObserver
-  ) 
-  {}
+  hide = true;
+  usuario: Usuario = new Usuario()
+  tDocs : TipoDocumento[] = [];
+  day = new Date()
+  min = new Date(new Date().getFullYear() - 80, 0, 1);
+  max = new Date(new Date().getFullYear() - 18, new Date().getMonth(), this.day.getDate());
 
-  ngOnInit(){
-    this.mediasub = this.mediaObserver.media$.subscribe((result:MediaChange) => {
-      this.devices = result.mqAlias === 'xs' ? true : false;   
-    })
+  actualizarForm=this.fb.group({
+    tipoDocumento:['',Validators.required],
+    numberIdentifi:['',Validators.required],
+    nameUser:['',Validators.required],
+    dateUser:['',Validators.required],
+    telefono:['', Validators.required],
+    direccion:[this.usuario.direccion, Validators.required],
+    password:['', Validators.required],
+    email:['',Validators.required]
+  });
+
+  constructor(private api_service :UsuarioService,private dropdownService: DropDownService,
+    private fb: FormBuilder,
+    ) { }
+
+  ngOnInit() {
+    this.getAltDocumentos();
+    this.getUser();
   }
 
-  ngOnDestroy(){
-    this.mediasub?.unsubscribe();
+  ngOnDestroy() {
+
   }
-  
+  getUser(){
+    this.api_service.getUsuarioById().subscribe(data=>{
+      this.usuario=data
+      console.info(" usaurio : ",data)
+    });
+  }
+ getAltDocumentos(){
+  this.dropdownService.getTipoDoc().subscribe(tipoDocs => {
+    this.tDocs = tipoDocs
+  })
+ }
+
+ submitActualizar(){
+  this.usuario.numeroIdentificacion = this.actualizarForm.value.numberIdentifi ? 0 : this.usuario.numeroIdentificacion;
+  this.usuario.direccion = this.actualizarForm.value.direccion ? '' :this.usuario.direccion;
+  this.usuario.email = this.actualizarForm.value.email ? '' : this.usuario.email;
+  this.usuario.nombre = this.actualizarForm.value.nameUser ? '' :this.usuario.nombre  ;
+  this.usuario.telefono = this.actualizarForm.value.telefono?'':this.usuario.telefono;
+  this.usuario.fechaNacimiento = this.actualizarForm.value.dateUser?'':this.usuario.fechaNacimiento;
+
+   console.info(this.usuario);
+
+ }
 }
